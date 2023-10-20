a3e3a8f9043052a729e3d1f16c908086b75cf2bf
Saksham <123803359+SakshamDev2005@users.noreply.github.com>
2023-10-20 17:36:08 +0530

    Update SyBot(VA).py

--- SyBot(VA).py
+++ SyBot(VA).py
@@ -2,17 +2,11 @@ import speech_recognition as sr
 import pyttsx3 as ps
 import google.generativeai as palm
 
+engine = ps.init()
+sr1 = sr.Recognizer()
+
 class Voice_Assistant:
-    def mic():
-        sr1 = sr.Recognizer()
-        with sr.Microphone() as source:
-            print('Listening....')
-            message = sr1.listen(source)
-            response = sr1.recognize_google(message , language = 'en-in')
-            print(response)
-
-            Voice_Assistant.AI(response)
-
+    
     def AI(x):
         palm.configure(api_key='')
         models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
@@ -25,13 +19,31 @@ class Voice_Assistant:
                 max_output_tokens = 150,
                 )
 
-        Voice_Assistant.speak(completion)
+        Voice_Assistant.speak(completion.result)
 
     def speak(y):
-        print(y.result)
-        engine = ps.init()
-        engine.say(y.result)
+        print(f'SyBot: {y}')
+        engine.say(y)
         engine.runAndWait()
 
 while True:
-    Voice_Assistant.mic()
+    with sr.Microphone() as source:
+        print('Listening....')
+        try:
+            message = sr1.listen(source)
+            response = sr1.recognize_google(message , language = 'en-in')
+
+            if response.lower() in ['stop','bye','goodbye','close']:
+                print('Ok then, Bye')
+                engine.say('Ok then, bye')
+                engine.runAndWait()
+                break
+    
+            else:
+                print(f'User: {response}')
+                Voice_Assistant.AI(response)
+                    
+        except:
+            print('Error, try again')
+            engine.say('Error, try again')
+            engine.runAndWait()
